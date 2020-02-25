0605e9f876e06b11cd406db965c2d969523f5555
Dewanshu-Chhatlani <dewanshu.chhatlani@diaspark.com>
2020-02-25 16:24:40 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import "./App.css";
+import Counters from "./components/counters";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0, disabled: true },
+      { id: 2, value: 0, disabled: true },
+      { id: 3, value: 0, disabled: true },
+      { id: 4, value: 0, disabled: true }
+    ]
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counter[index] = { ...counter };
+    counters[index].value++;
+    if (counter.value > 0) {
+      counters[index].disabled = false;
+    }
+    this.setState({ counters });
+  };
+
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counter[index] = { ...counter };
+    counters[index].value--;
+    if (counter.value === 0) {
+      counters[index].disabled = true;
+    }
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      c.disabled = true;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = counterID => {
+    const counters = this.state.counters.filter(c => c.id !== counterID);
+    this.setState({ counters });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
